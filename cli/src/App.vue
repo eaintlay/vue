77632247cd0d2fd30771dec1ea93f2bc886bee22
eaintlay <haythi315811@gmail.com>
2020-09-03 14:24:15 +0630

    message

---
+++ cli/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Testing msg="This is inherient data"></Testing>
+    
+  </div>
+</template>
+
+<script>
+import Testing from './components/Testing.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Testing
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
