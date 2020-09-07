2a066975efa74e7c0282376541519a05b4562bfc
eaintlay <haythi315811@gmail.com>
2020-09-08 01:06:22 +0630

    message

--- cli/src/App.vue
+++ cli/src/App.vue
@@ -3,31 +3,29 @@
     <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="container">
-          <a class="navbar-brand" href="#">
-            <img src="./assets/logo.png" width="30" height="30" alt="" loading="lazy">
-          </a>
-          <button class="navbar-toggler" type="button" data-toggle="        collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
+        <router-link class="navbar-brand" to="/">
+          <img src="./assets/logo.png" width="30" height="30" alt="" loading="lazy">
+        </router-link>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
 
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-              <li class="nav-item active">
-                <router-link class="nav-link" to="/testing">Testing<span class="sr-only">(current)</span></router-link>
-              </li>
-              <li class="nav-item">
-                <router-link class="nav-link" to="/exercise">Exercise</router-link>
-              </li>
-            </ul>
-          </div>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <router-link class="nav-link" to="/testing">Testing <span class="sr-only">(current)</span></router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/exercise">Exercise</router-link>
+            </li>
+          </ul>
         </div>
-      </nav>
+      </div>
+    </nav>
     <div class="container">
-      
-      <div class="row">
+      <div class="row my-3">
         <div class="col-md-12">
           <router-view></router-view>
-          
         </div>
         <!-- <div class="col-md-6">
           <Testing msg="This is inherient data"></Testing>
@@ -35,11 +33,8 @@
         <div class="col-md-6">
           <Exercise></Exercise>
         </div> -->
-       </div>
       </div>
-  </div>
-    
-    
+    </div>
   </div>
 </template>
 
@@ -48,7 +43,7 @@
 export default {
   name: 'App',
   mounted(){
-    this.$router.replace('/testing')
+      // this.$router.replace('/home')
   }
 }
 </script>
@@ -60,6 +55,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top:0;
+  margin-top: 0;
 }
 </style>
